e0c64477c76ad75a86d7a2181a9e8ebe1f51305a
KarolinaWrobel <karolinaczerczak@gmail.com>
2022-12-13 18:17:50 +0100

    dodanie kolejnej ścieżki pliku

--- lib/const.py
+++ lib/const.py
@@ -1,3 +1,4 @@
 DATA_DIR = '/content/gdrive/.shortcut-targets-by-id/1QtpqeNnbJx1Wz9AxPPOQVNNAtv8wKdu5/bigdataaktorzy'
 JOINED_DATA = f'{DATA_DIR}/joined_data.parquet'
 DATA_WITH_AWARDS = f'{DATA_DIR}/data_with_awards.parquet'
+DATA_WITH_NUMBER_OF_AWARDS = f'{DATA_DIR}/data_with_number_of_awards.parquet'
