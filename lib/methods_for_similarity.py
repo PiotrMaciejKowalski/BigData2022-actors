745cf89a19e84a447ce3b686799780060a03bb62
Jakub Deka <pracownik211086@gmail.com>
2022-12-13 16:43:47 +0100

    poprawiono metode i usunieto notatnik

--- lib/methods_for_similarity.py
+++ lib/methods_for_similarity.py
@@ -1,8 +1,8 @@
-import numpy as np
-
-def IOU(lista1, lista2):
-  # metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]
-  intersection = np.intersect1d(lista1, lista2)
-  union = np.union1d(lista1, lista2)
-  IOU = len(intersection)/len(union)
-  return IOU
+def pokrycie_przedzialow(przedzial1, przedzial2):
+  # metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
+  # metoda zwraca liczbę z przedziału [0, 1]
+  min_l = min(przedzial1[0], przedzial2[0])
+  max_l = max(przedzial1[0], przedzial2[0])
+  min_p = min(przedzial1[1], przedzial2[1])
+  max_p = max(przedzial1[1], przedzial2[1])
+  return (min_p - max_l)/(max_p - min_l)
