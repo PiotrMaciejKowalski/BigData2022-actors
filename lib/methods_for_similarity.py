c55e2b9ad9b3469a09ad4959b82d43e88d8bb10a
Jakub Deka <pracownik211086@gmail.com>
2022-12-14 19:00:24 +0100

    dodano typy i uwzgledniono puste listy

--- lib/methods_for_similarity.py
+++ lib/methods_for_similarity.py
@@ -1,18 +1,15 @@
-def iou(lista1, lista2):
+def iou(lista1: list, lista2: list) -> float:
   # metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]
-  intersection = []
-  union = []
-  for element in lista1:
-    union.append(element)
-    if element in lista2:
-      intersection.append(element)
-  for element in lista2:
-    if element not in union:
+  if not lista1 or not lista2:
+    return 0
+  else:
+    intersection = []
+    union = []
+    for element in lista1:
       union.append(element)
-  return len(intersection)/len(union)
-
-# test jednostkowy dla metody iou
-lista1 = [1, 3, 5, 7, 9]
-lista2 = [1, 4, 7, 10]
-
-print(iou(lista1, lista2))
+      if element in lista2:
+        intersection.append(element)
+    for element in lista2:
+      if element not in union:
+        union.append(element)
+    return len(intersection)/len(union)
