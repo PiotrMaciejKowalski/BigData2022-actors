7f29c2a69873a004aed1347af933282fe6134f7a
Jakub Deka <pracownik211086@gmail.com>
2022-12-14 19:06:27 +0100

    dodano typy i uwzgledniono listy nie bedace przedzialami

--- lib/methods_for_similarity.py
+++ lib/methods_for_similarity.py
@@ -1,8 +1,11 @@
-def pokrycie_przedzialow(przedzial1, przedzial2):
+def pokrycie_przedzialow(przedzial1: list, przedzial2: list) -> float:
   # metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
   # metoda zwraca liczbę z przedziału [0, 1]
-  min_l = min(przedzial1[0], przedzial2[0])
-  max_l = max(przedzial1[0], przedzial2[0])
-  min_p = min(przedzial1[1], przedzial2[1])
-  max_p = max(przedzial1[1], przedzial2[1])
-  return (min_p - max_l)/(max_p - min_l)
+  if len(przedzial1) != 2 or len(przedzial2) != 2:
+    return 0
+  else:
+    min_l = min(przedzial1[0], przedzial2[0])
+    max_l = max(przedzial1[0], przedzial2[0])
+    min_p = min(przedzial1[1], przedzial2[1])
+    max_p = max(przedzial1[1], przedzial2[1])
+    return (min_p - max_l)/(max_p - min_l)
