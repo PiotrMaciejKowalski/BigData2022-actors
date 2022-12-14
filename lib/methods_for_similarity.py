bad9491f1d433aec375a36b8f1afb5ecbdd7755f
Jakub Deka <pracownik211086@gmail.com>
2022-12-14 18:11:47 +0100

    zmiana liczenia i dodanie testu jednostkowego

--- lib/methods_for_similarity.py
+++ lib/methods_for_similarity.py
@@ -1,8 +1,18 @@
-import numpy as np
-
-def IOU(lista1, lista2):
+def iou(lista1, lista2):
   # metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]
-  intersection = np.intersect1d(lista1, lista2)
-  union = np.union1d(lista1, lista2)
-  IOU = len(intersection)/len(union)
-  return IOU
+  intersection = []
+  union = []
+  for element in lista1:
+    union.append(element)
+    if element in lista2:
+      intersection.append(element)
+  for element in lista2:
+    if element not in union:
+      union.append(element)
+  return len(intersection)/len(union)
+
+# test jednostkowy dla metody iou
+lista1 = [1, 3, 5, 7, 9]
+lista2 = [1, 4, 7, 10]
+
+print(iou(lista1, lista2))
