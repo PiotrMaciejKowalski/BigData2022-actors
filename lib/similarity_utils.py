f3b4f983f0ef7c2fa6c71f28919ed06e9f0c25e1
Jakub Deka <pracownik211086@gmail.com>
2022-12-17 12:12:33 +0100

    Poprawiono doc string

--- lib/similarity_utils.py
+++ lib/similarity_utils.py
@@ -1,5 +1,6 @@
 def iou(lista1: list[any], lista2: list[any]) -> float:
-  '''metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]'''
+  ''' metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]
+  '''
   if not lista1 or not lista2:
     return 0
   else:
