cbccf02644ae915ba4dd9943f89942a874fada0a
Jakub Deka <pracownik211086@gmail.com>
2022-12-17 12:10:20 +0100

    Poprawiono doc string

--- lib/methods_for_similarity.py
+++ lib/methods_for_similarity.py
@@ -1,6 +1,6 @@
-def pokrycie_przedzialow(przedzial1: list, przedzial2: list) -> float:
-  # metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
-  # metoda zwraca liczbę z przedziału [0, 1]
+def pokrycie_przedzialow(przedzial1: list[int], przedzial2: list[int]) -> float:
+  ''' metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
+  metoda zwraca liczbę z przedziału [0, 1] '''
   if len(przedzial1) != 2 or len(przedzial2) != 2:
     return 0
   elif przedzial1[1] <= przedzial1[0] or przedzial2[1] <= przedzial2[0]:
