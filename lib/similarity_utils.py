7023a593cd63cf4c32e75a055293691c421b6355
Jakub Deka <pracownik211086@gmail.com>
2022-12-17 11:07:14 +0100

    usuniecie testow z pliku utils

--- lib/similarity_utils.py
+++ lib/similarity_utils.py
@@ -8,9 +8,3 @@ def iou(lista1: list, lista2: list) -> float:
     intersection = set(lista1).intersection(set(lista2))
     union = set(lista1).union(set(lista2))
     return len(intersection)/len(union)
-    
-
-# test jednostkowy dla metody iou
-print('1st IOU ', iou([1, 3, 5, 7, 9], [1, 4, 7, 10]))
-print('2nd IOU ', iou([], [3, 6]))
-print('3rd IOU ', iou([], []))
