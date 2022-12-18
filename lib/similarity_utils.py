be9babd12a5d38116ee9bf02fb902c43ff7ce814
Jakub Deka <pracownik211086@gmail.com>
2022-12-18 11:33:05 +0100

    Poprawiono typing i dodano asserty

--- lib/similarity_utils.py
+++ lib/similarity_utils.py
@@ -1,13 +1,13 @@
-def pokrycie_przedzialow(przedzial1: list[int], przedzial2: list[int]) -> float:
-  ''' metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
-  metoda zwraca liczbę z przedziału [0, 1] '''
-  if len(przedzial1) != 2 or len(przedzial2) != 2:
-    return 0
-  elif przedzial1[1] <= przedzial1[0] or przedzial2[1] <= przedzial2[0]:
-    return 0
-  else:
+from typing import List
+
+
+def pokrycie_przedzialow(przedzial1: List[int], przedzial2: List[int]) -> float:
+    ''' metoda przyjmuje jako argument dwie listy w postaci przedzialu liczbowego, tj. lista = [a, b], gdzie a <= b
+    metoda zwraca liczbę z przedziału [0, 1] '''
+    assert len(przedzial1) == 2 and len(przedzial2) == 2, 'dlugosc przedzialu powinna wynosic 2'
+    assert przedzial1[1] >= przedzial1[0] and przedzial2[1] >= przedzial2[0], 'przedzial [a, b] jest niepoprawny (b<a)'
     min_l = min(przedzial1[0], przedzial2[0])
     max_l = max(przedzial1[0], przedzial2[0])
     min_p = min(przedzial1[1], przedzial2[1])
     max_p = max(przedzial1[1], przedzial2[1])
-    return (min_p - max_l)/(max_p - min_l)
+    return (min_p - max_l) / (max_p - min_l)
