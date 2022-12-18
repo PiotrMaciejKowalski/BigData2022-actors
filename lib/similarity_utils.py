e1fe237c1375748651b0320eaebf2f1e40e73aaf
PiotrMaciejKowalski <piotr.maciej.kowalski.developer@gmail.com>
2022-12-18 07:57:17 +0100

    fixing problem in iou pull request

--- lib/similarity_utils.py
+++ lib/similarity_utils.py
@@ -1,11 +1,13 @@
-def iou(lista1: list[any], lista2: list[any]) -> float:
-  ''' metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]
-  '''
-  if not lista1 or not lista2:
-    return 0
-  else:
-    intersection = []
-    union = []
-    intersection = set(lista1).intersection(set(lista2))
-    union = set(lista1).union(set(lista2))
-    return len(intersection)/len(union)
+from typing import List, Any
+
+
+def iou(lista1: List[Any], lista2: List[Any]) -> float:
+    """metoda przyjmuje jako argument dwie listy i zwraca ich indeks Jaccarda, czyli liczbę z przedzialu [0,1]"""
+    if not lista1 or not lista2:
+        return 0
+    else:
+        intersection = []
+        union = []
+        intersection = set(lista1).intersection(set(lista2))
+        union = set(lista1).union(set(lista2))
+        return len(intersection) / len(union)
