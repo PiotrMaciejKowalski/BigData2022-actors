9512286ae848e57949294afde404b864ae3cfe4c
Pawel180 <116296821+Pawel180@users.noreply.github.com>
2022-12-19 10:46:52 +0100

    Update metrics.py

--- lib/metrics.py
+++ lib/metrics.py
@@ -1,4 +1,7 @@
 def simple_metric(rankings: Dict[str, List[str],query_data: pd.DataFrame, reference_data: pd.DataFrame) ➝ float:
+    result = 0
+    suma = 0
+    pozycja = 0
 """
 rankings - odwzorowanie, które gromadzi stałe top n-rekomendacji. Same n może zostać wydedukowane z rozmiaru list, który musi być stały
 query_data - dane ze zbioru testowanych aktorów, to nie muszą być te same kolumny, które biorą udział w trenowaniu. To są dane aktorów dla których wyznaczane są rekomendacje.
