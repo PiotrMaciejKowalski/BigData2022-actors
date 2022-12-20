c87ff997d5eebc515b8f9bbd6a0fcd879d967180
Pawel180 <116296821+Pawel180@users.noreply.github.com>
2022-12-20 01:57:18 +0100

    Update metrics.py

--- lib/metrics.py
+++ lib/metrics.py
@@ -1,7 +1,15 @@
-def simple_metric(rankings: Dict[str, List[str],query_data: pd.DataFrame, reference_data: pd.DataFrame) ➝ float:
+def simple_metric(rankings: Dict[str, List[str], query_data: pd.DataFrame, reference_data: pd.DataFrame) ➝ float:
     result = 0
     suma = 0
     pozycja = 0
+    for actor in rankings:
+        for other_actor in rankings[actor]:
+            licznik = 0
+            if query_data['category']where(query_data['nconst'] = actor) = reference_data['category']where(reference_data['nconst'] = other_actor):
+                licznik += 1
+            sum += licznik/max_licznik * (len(lista_id_aktorow) - pozycja)
+        
+                                 
 """
 rankings - odwzorowanie, które gromadzi stałe top n-rekomendacji. Same n może zostać wydedukowane z rozmiaru list, który musi być stały
 query_data - dane ze zbioru testowanych aktorów, to nie muszą być te same kolumny, które biorą udział w trenowaniu. To są dane aktorów dla których wyznaczane są rekomendacje.
