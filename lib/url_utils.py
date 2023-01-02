b7796ca69a9508b901f36ef1b2e16d0f49a4babe
Jakub Deka <pracownik211086@gmail.com>
2023-01-02 13:25:24 +0100

    dodanie metody z if w przycinaniu URL

--- lib/url_utils.py
+++ lib/url_utils.py
@@ -8,11 +8,22 @@ from pyspark.sql.functions import udf
 def extract_link_to_image(text: str) -> str:
     try:
         first = 'src="'
-        last = "._V1"
+        last = "._"
         start = text.rindex(first) + len(first)
         end = text.rindex(last, start)
         return text[start:end]
     except ValueError:
+        return None
+
+
+def if_extract_link_to_image(text: str) -> str:
+    if text is not None:
+        first = 'src="'
+        last = "._"
+        start = text.rindex(first) + len(first)
+        end = text.rindex(last, start)
+        return text[start:end]
+    else:
         return None
 
 
@@ -25,6 +36,17 @@ def get_link_to_imdb_image(actor_id: str) -> str:
 
 
 udf_get_link_to_image = udf(get_link_to_imdb_image)
+
+
+def if_get_link_to_imdb_image(actor_id: str) -> str:
+    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    image_info = str(soup.find("img", attrs={"class": "poster"}))
+    return extract_link_to_image(image_info)
+
+
+udf_if_get_link_to_image = udf(if_get_link_to_imdb_image)
 
 
 def alt_get_link_to_imdb_image(actor_id: str) -> str:
