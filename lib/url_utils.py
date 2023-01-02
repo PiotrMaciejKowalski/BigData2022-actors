4b9ee2237ee549ce9206d5c44e1a92431598b39b
Jakub Deka <pracownik211086@gmail.com>
2023-01-02 13:16:05 +0100

    Dodanie alternatywnej metody

--- lib/url_utils.py
+++ lib/url_utils.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from lxml import html
 
 from pyspark.sql.functions import udf
 
@@ -24,3 +25,19 @@ def get_link_to_imdb_image(actor_id: str) -> str:
 
 
 udf_get_link_to_image = udf(get_link_to_imdb_image)
+
+
+def alt_get_link_to_imdb_image(actor_id: str) -> str:
+    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    poster_el = tree.find('.//img[@class="poster"]')
+    if poster_el is not None:
+        return poster_el.attrib['src'].split("._")[0]
+    else:
+        return None
+
+
+udf_alt_get_link_to_image = udf(alt_get_link_to_imdb_image)
+# do ruuchomienia tej metody potrzebna będzie doinstalowanie cssselect za pomocą poniższego
+# !sudo pip3 install cssselect
