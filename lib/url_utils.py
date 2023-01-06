0e138c6cacc3bb55e6ba69289d46c3963084d6b6
Jakub Deka <pracownik211086@gmail.com>
2023-01-06 10:37:36 +0100

    dodanie metod z parserami

--- lib/url_utils.py
+++ lib/url_utils.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
-from html5lib import HTMLParser
+# from html5lib import HTMLParser
 # from pyquery import PyQuery as pq
 from pyspark.sql.functions import udf
 
@@ -50,32 +50,46 @@ def if_get_link_to_imdb_image(actor_id: str) -> str:
 udf_if_get_link_to_image = udf(if_get_link_to_imdb_image)
 
 
-def get_link_to_imdb_image(actor_id: str) -> str:
+def get_link_to_imdb_image_html(actor_id: str) -> str:
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    image_info = str(soup.get("img", attrs={"class": "poster"}))
+    image_info = soup.find("img", attrs={"class": "poster"})
     if image_info is not None:
         return image_info.get("src").split("._")[0]
     else:
         return None
 
 
-udf_get_link_to_image = udf(get_link_to_imdb_image)
+udf_get_link_to_imdb_image_html = udf(get_link_to_imdb_image_html)
 
 
-def find_link_to_imdb_image(actor_id: str) -> str:
+def get_link_to_imdb_image_lxml(actor_id: str) -> str:
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    image_info = str(soup.find("img", attrs={"class": "poster"}))
+    soup = BeautifulSoup(response.content, "lxml")
+    image_info = soup.find("img", attrs={"class": "poster"})
     if image_info is not None:
         return image_info.get("src").split("._")[0]
     else:
         return None
 
 
-udf_find_link_to_image = udf(find_link_to_imdb_image)
+udf_get_link_to_imdb_image_lxml = udf(get_link_to_imdb_image_lxml)
+
+
+def get_link_to_imdb_image_html5lib(actor_id: str) -> str:
+    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html5lib")
+    image_info = soup.find("img", attrs={"class": "poster"})
+    if image_info is not None:
+        return image_info.get("src").split("._")[0]
+    else:
+        return None
+
+
+udf_get_link_to_imdb_image_html5lib = udf(get_link_to_imdb_image_html5lib)
 
 
 def lxml_get_link_to_imdb_image(actor_id: str) -> str:
@@ -94,16 +108,16 @@ udf_lxml_get_link_to_image = udf(lxml_get_link_to_imdb_image)
 # !sudo pip3 install cssselect
 
 
-def html_get_link_to_imdb_image(actor_id: str) -> str:
-    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
-    response = requests.get(url)
-    parser = HTMLParser()
-    soup = parser.parse(response.content)
-    image_info = str(soup.find("img", attrs={"class": "poster"}))
-    return extract_link_to_image(image_info)
+# def html_get_link_to_imdb_image(actor_id: str) -> str:
+#     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
+#     response = requests.get(url)
+#     parser = HTMLParser()
+#     soup = parser.parse(response.content)
+#     image_info = str(soup.find("img", attrs={"class": "poster"}))
+#     return extract_link_to_image(image_info)
 
 
-udf_html_get_link_to_image = udf(html_get_link_to_imdb_image)
+# udf_html_get_link_to_image = udf(html_get_link_to_imdb_image)
 
 
 # def pq_get_link_to_imdb_image(actor_id: str) -> str:
