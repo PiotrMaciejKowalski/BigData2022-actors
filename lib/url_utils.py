8c7bfed45948d775ddd3c73935281b41819e17c3
Jakub Deka <pracownik211086@gmail.com>
2023-01-11 20:02:26 +0100

    dodanie wszystkich testowanych metod i ich opisów

--- lib/url_utils.py
+++ lib/url_utils.py
@@ -1,18 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from lxml import html
-# from html5lib import HTMLParser
-# from pyquery import PyQuery as pq
 from pyspark.sql.functions import udf
+from lxml import html
 
-
-# metody z biblioteki beautiful soup
-
-## metody wycianjące link z pewnej części strony
-
-### wersja try
 
 def try_extract_link_to_image(text: str) -> str:
+    """metoda dla wejściowego tekstu zwraca część tego tekst znajdującą się pomiędzy sekwencjami znaków 'src="' i "._";
+    metoda obsługuje wyjątki poprzez zwrócenie elemntu None"""
     try:
         first = 'src="'
         last = "._"
@@ -24,6 +18,9 @@ def try_extract_link_to_image(text: str) -> str:
 
 
 def try_get_link_to_imdb_image(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, korzystając z metody try_extract_link_to_image do ucięcia części linku odpowiadającej za
+    formatowanie"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -33,9 +30,9 @@ def try_get_link_to_imdb_image(actor_id: str) -> str:
 
 udf_try_get_link_to_imdb_image = udf(try_get_link_to_imdb_image)
 
-### wersja if
 
 def extract_link_to_image(text: str) -> str:
+    """metoda zwraca tekst znajdujący się pomiędzy sekwencjami znaków 'src="' oraz '._'"""
     text = str(text)
     first = 'src="'
     last = "._"
@@ -45,6 +42,10 @@ def extract_link_to_image(text: str) -> str:
 
 
 def if_get_link_to_imdb_image(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, korzystając z metody extract_link_to_image do ucięcia części linku odpowiadającej za formatowanie
+    zdjęcia;
+    gdy link do zdjęcia jest pusty, tj. aktor nie ma zdjęcia, to metoda zwraca element None"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -56,82 +57,96 @@ def if_get_link_to_imdb_image(actor_id: str) -> str:
 
 udf_if_get_link_to_imdb_image = udf(if_get_link_to_imdb_image)
 
-## metody docinające końcówkę linku wydobytego z właściwego elemntu (zdjęcia)
-
-### wersja try
-
-def if_get_link_to_imdb_image_src(actor_id: str) -> str:
-    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    image_info = soup.find("img", attrs={"class": "poster"})
-    if image_info is not None:
-        return image_info.get("src").split("._")[0]
-    else:
-        return image_info.get("src").split("._")[0]
-
-
-udf_if_get_link_to_imdb_image_src = udf(if_get_link_to_imdb_image_src)
-
-### wersja if
 
 def try_get_link_to_imdb_image_src(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, poprzez odwołanie się do odpowiedniego elementu src na stronie oraz następne przycięcie linku przed
+    wystąpieniem sekwencji znaków "._";
+    metoda obsługuje wyjątki gdy link jest pusty poprzez zwrócenie elemntu None"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     image_info = soup.find("img", attrs={"class": "poster"})
+    print(image_info)
     try:
         return image_info.get("src").split("._")[0]
-    except ValueError:
+    except AttributeError:
         return None
 
 
 udf_try_get_link_to_imdb_image_src = udf(try_get_link_to_imdb_image_src)
 
 
-## testowanie innych parserów
+def if_get_link_to_imdb_image_src(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, poprzez odwołanie się do odpowiedniego elementu src na stronie oraz następne przycięcie linku przed
+    wystąpieniem sekwencji znaków "._";
+    metoda w przypadku gdy link jest pusty zwraca elemnt None;
+    używany parser to 'html.parser'"""
+    url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    image_info = soup.find("img", attrs={"class": "poster"})
+    print(image_info)
+    if image_info is not None:
+        return image_info.get("src").split("._")[0]
+    else: return None
 
-def get_link_to_imdb_image_lxml(actor_id: str) -> str:
+
+udf_if_get_link_to_imdb_image_src = udf(if_get_link_to_imdb_image_src)
+
+
+def if_get_link_to_imdb_image_lxml(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, poprzez odwołanie się do odpowiedniego elementu src na stronie oraz następne przycięcie linku przed
+    wystąpieniem sekwencji znaków "._";
+    metoda w przypadku gdy link jest pusty zwraca elemnt None;
+    używany parser to 'lxml'"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "lxml")
     image_info = soup.find("img", attrs={"class": "poster"})
+    print(image_info)
     if image_info is not None:
         return image_info.get("src").split("._")[0]
-    else:
-        return None
+    else: return None
 
 
-udf_get_link_to_imdb_image_lxml = udf(get_link_to_imdb_image_lxml)
+udf_if_get_link_to_imdb_image_lxml = udf(if_get_link_to_imdb_image_lxml)
 
 
-def get_link_to_imdb_image_html5lib(actor_id: str) -> str:
+def if_get_link_to_imdb_image_html5lib(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    BeautifulSoup, poprzez odwołanie się do odpowiedniego elementu src na stronie oraz następne przycięcie linku przed
+    wystąpieniem sekwencji znaków "._";
+    metoda w przypadku gdy link jest pusty zwraca elemnt None;
+    używany parser to 'html5lib'"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html5lib")
     image_info = soup.find("img", attrs={"class": "poster"})
+    print(image_info)
     if image_info is not None:
         return image_info.get("src").split("._")[0]
-    else:
-        return None
+    else: return None
 
 
-udf_get_link_to_imdb_image_html5lib = udf(get_link_to_imdb_image_html5lib)
+udf_if_get_link_to_imdb_image_html5lib = udf(if_get_link_to_imdb_image_html5lib)
 
-
-# metody z biblioteki lxml
 
 def lxml_get_link_to_imdb_image(actor_id: str) -> str:
+    """metoda przyjmuje jako argument id aktora ze strony IMDB i zwraca link do jego/jej zdjęcia za pomocą biblioteki
+    lxml, poprzez odwołanie się do odpowiedniego elementu src na stronie oraz następne przycięcie linku przed
+    wystąpieniem sekwencji znaków "._";
+    do działania biblioteki wymagane jest zainstalowanie cssselect za pomocą poniższej linijki z kodem:
+    !sudo pip3 install cssselect"""
     url = f"https://www.imdb.com/name/{actor_id}/mediaindex"
     page = requests.get(url)
     tree = html.fromstring(page.content)
     poster_el = tree.find('.//img[@class="poster"]')
     if poster_el is not None:
         return poster_el.attrib['src'].split("._")[0]
-    else:
-        return None
+    else: return None
 
 
 udf_lxml_get_link_to_image = udf(lxml_get_link_to_imdb_image)
-# do ruuchomienia tej metody potrzebna będzie doinstalowanie cssselect za pomocą poniższego
-# !sudo pip3 install cssselect
