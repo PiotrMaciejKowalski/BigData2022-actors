a824408b42a73ff5ec59bbfb62d60ce62ba8af21
Emilia Kotlarska <116435566+emiliakotlarska@users.noreply.github.com>
2023-01-12 18:07:10 +0100

    Update adding_columns.py

--- lib/adding_columns.py
+++ lib/adding_columns.py
@@ -95,18 +95,11 @@ def add_average_films_ratings(spark: SparkSession, data: DataFrame) -> DataFrame
     return data
 
 def add_top_type(data: DataFrame) -> DataFrame:
-        df2 = data.select('nconst', 
-                          explode(data.titleType).alias('titleType')
-                         )
-        df3 = df2.select('nconst', 
-                         explode(split(df2.titleType, ',')).alias('titleType')
-                        )
+        df2 = data.select('nconst', explode(data.titleType).alias('titleType'))
+        df3 = df2.select('nconst', explode(split(df2.titleType, ',')).alias('titleType'))
         w = Window.partitionBy('nconst', 'titleType')
-        aggregated_table = df3.withColumn(
-            "count", count("*").over(w)).withColumn(
-            "rn", row_number().over(w.orderBy(desc("count")))).filter(
-            "rn = 1").groupBy('nconst').agg(first('titleType').alias('top_type')
-                                          )
+        aggregated_table = df3.withColumn("count", count("*").over(w)).withColumn(
+            "rn", row_number().over(w.orderBy(desc("count")))).filter("rn = 1").groupBy('nconst').agg(first('titleType').alias('top_type'))
         data = data.join(aggregated_table, on="nconst", how="left")
         return data
 
