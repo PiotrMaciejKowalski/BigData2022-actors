3554bb60fae92bbcb5b67ba904a1bf27fd5df8d3
Emilia Kotlarska <116435566+emiliakotlarska@users.noreply.github.com>
2023-01-19 15:28:31 +0100

    Update adding_columns.py

--- lib/adding_columns.py
+++ lib/adding_columns.py
@@ -6,6 +6,16 @@ from pyspark.sql import Window
 
 from lib.pyspark_init import load_ratings_data
 
+from pandas import DataFrame
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.ml import Pipeline
+from pyspark.ml.pipeline import Transformer
+from pyspark.ml.feature import MinMaxScaler, VectorAssembler
+from pyspark.sql.types import DoubleType
+from pyspark.sql.functions import explode, col, count, avg, udf
+
+from lib.pyspark_init import load_ratings_data
+
 
 def add_number_of_oscars(data: DataFrame) -> DataFrame:
     oscars_nominations = (
@@ -25,6 +35,7 @@ def add_number_of_oscars(data: DataFrame) -> DataFrame:
 
     data = data.join(oscars_nominations, on="nconst", how="left")
     data = data.join(oscars_win, on="nconst", how="left")
+    data = data.na.fill(value = 0, subset = ["no_nominations_oscars", "no_oscars"])
     return data
 
 
@@ -46,6 +57,7 @@ def add_number_of_globes(data: DataFrame) -> DataFrame:
 
     data = data.join(globes_nominations, on="nconst", how="left")
     data = data.join(globes_win, on="nconst", how="left")
+    data = data.na.fill(value = 0, subset = ["no_nominations_globes", "no_globes"])
     return data
 
 
@@ -67,6 +79,7 @@ def add_number_of_emmy_awards(data: DataFrame) -> DataFrame:
 
     data = data.join(emmy_nominations, on="nconst", how="left")
     data = data.join(emmy_win, on="nconst", how="left")
+    data = data.na.fill(value = 0, subset = ["no_nominations_emmy", "no_emmy"])
     return data
 
 
@@ -78,6 +91,7 @@ def add_number_of_films(data: DataFrame) -> DataFrame:
         .select(["nconst", "no_films"])
     )
     data = data.join(number_of_films, on="nconst", how="left")
+    data = data.na.fill(value = 0, subset = ["no_films"])
     return data
 
 
@@ -93,8 +107,31 @@ def add_average_films_ratings(spark: SparkSession, data: DataFrame) -> DataFrame
         .select(["nconst", "average_films_rating"])
     )
     data = data.join(data_with_ratings, on="nconst", how="left")
+    data = data.na.fill(value = 0, subset = ["average_films_rating"])
     return data
 
+
+class Normalized_column(Transformer):
+    def __init__(self, inputCol):
+        self.inputCol = inputCol
+    def this():
+        this(Identifiable.randomUID("normalizedcolumn"))
+    def copy(extra):
+        defaultCopy(extra)
+    def _transform(self, data):
+        unlist = udf(lambda x: round(float(list(x)[0]),3), DoubleType()) # zamiana typu wartości w kolumnie z wektora na Double
+        return data.withColumn(self.inputCol + "_norm", unlist(self.inputCol + "_norm")).drop(self.inputCol + "_Vect")
+
+def add_normalized_columns(data: DataFrame) -> DataFrame:
+    assert (x not in data.columns for x in ["no_nominations_oscars_norm", "no_oscars_norm", "no_nominations_globes_norm", "no_globes_norm", "no_nominations_emmy_norm", "no_emmy_norm", "no_films_norm", "average_films_rating_norm"])
+    to_be_normalized = ["no_nominations_oscars", "no_oscars", "no_nominations_globes", "no_globes", "no_nominations_emmy", "no_emmy", "no_films", "average_films_rating"]
+    for i in to_be_normalized:
+        assembler = VectorAssembler(inputCols = [i], outputCol = i + "_Vect") # zamiana wartości z bigint na wektory
+        scaler = MinMaxScaler(inputCol = i + "_Vect", outputCol = i + "_norm") # przeskalowanie wartości (funkcja działa na liście wektorów utworzonej z wartości w skalowanej kolumnie)
+        normalized_column = Normalized_column(inputCol = i)
+        pipeline = Pipeline(stages=[assembler, scaler, normalized_column])
+        data = pipeline.fit(data).transform(data)
+        
 def genres_code(data: DataFrame) -> DataFrame:
     assert "genres_code" not in data.columns
     genres = ('Crime', 'Romance', 'Thriller', 'Adventure', 'Drama', 'War', 'Documentary', 'Reality-TV', 'Family', 'Fantasy', 'Game-Show', 'Adult', 'History', 'Mystery', 'Experimental', 'Musical', 'Animation', 'Music', 'Film-Noir', 'Short', 'Horror', 'Western', 'Biography', 'Comedy', 'Action', 'Sport', 'Talk-Show', 'Sci-Fi', 'News')
@@ -121,8 +158,6 @@ def category_code(data: DataFrame) -> DataFrame:
     indexer_fitted = indexer.fit(data)
     data = indexer_fitted.transform(data)
     return data
-
-
 
 def add_top_type(data: DataFrame) -> DataFrame:
         df2 = data.select('nconst', explode(data.titleType).alias('titleType'))
@@ -165,12 +200,13 @@ def add_all_columns(spark: SparkSession, data: DataFrame) -> DataFrame:
     data = add_number_of_globes(data)
     data = add_number_of_emmy_awards(data)
     data = add_number_of_films(data)
-    data = add_average_films_ratings(spark, data)
+    data = add_average_films_ratings(spark, data).cache() # .cache() przyspieszy działanie kolejnej funkcji - dane po wykonaniu funkcji add_average_films_ratings są zapamiętywane i przechowywane, dzięki czemu przy kolejnej funkcji wszystkie wcześniejsze operacje nie muszą być wykonywane przy każdej komendzie wymagającej tych danych
+    data = add_normalized_columns(data)
     data = genres_code(data)
     data = types_code(data)
     data = category_code(data)
-    data = add_top_type(data)
-    data = add_top_genres(data)
-    data = add_top2_type(data)
     data = add_top0_genres(data)
+    data = add_top_genres(data)
+    data = add_top_type(data)
+    data = add_top2_type(data)
     return data
