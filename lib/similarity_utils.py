826bfff82daf5c4f5ba361f179bde2b1228e1cdc
Jakub Deka <pracownik211086@gmail.com>
2023-01-22 13:21:18 +0100

    dodanie notki o testach metod

--- lib/similarity_utils.py
+++ lib/similarity_utils.py
@@ -151,7 +151,9 @@ def insert_main_actor_column_values(data: pd.DataFrame, column_name: str, value:
 
 
 def similarity_pandas(row: pd.DataFrame) -> float:
-    """metoda liczy similarity pomiędzy dwoma aktorami znadjującymi się w tym samym wierszu;
+    """metoda została swtorzona do testów! sugerowane jest używanie metody 'similarity'!
+
+    metoda liczy similarity pomiędzy dwoma aktorami znadjującymi się w tym samym wierszu;
     aby dodać do wyciągniętego z załadowanych przez nas danych aktora (który jest podany jako pd.DataFrame) kolumn z wartościami aktora
     względem którego będzie liczone similarity, należy użyć metody 'insert_main_actor_column_values' na poniższych kolumnach:
     "tconst", "titleType", "genres", "category"
@@ -170,7 +172,9 @@ def similarity_pandas(row: pd.DataFrame) -> float:
 
 
 def similarity_one_vs_all_pandas(data: pd.DataFrame, main_actor_values: pd.DataFrame) -> Tuple[List[str], List[float]]:
-    """metoda liczy similarity pomiędzy aktorem dla którego dane są dostarczone w postaci pandasowego wiersza, oraz
+    """metoda została swtorzona do testów! sugerowane jest używanie metody 'similarity_one_vs_all'!
+
+    metoda liczy similarity pomiędzy aktorem dla którego dane są dostarczone w postaci pandasowego wiersza, oraz
     wszystkimi aktorami obecnymi w ramce danych data;
     w pojedynczym wierszu znajdują się informacje o jednym aktorze pierwotnie znajdującym się w wierszu, oraz obok tych
     danych doklejane są dane o aktorze względem którego liczone jest similarity;
